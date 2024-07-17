65693f8a0e43bdbcce938383f6e0cad299a77d5d
Edson Mizuma <edson.mizuma@gmail.com>
2024-07-17 16:30:18 -0300

    criação da classe mãe Conteúdo e alteração das classes filhas

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -7,17 +8,32 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        Curso curso1 = new Curso("Curso Java", "Descrição curso Java", 8);
-        Curso curso2 = new Curso("Curso JS", "Descrição curso JS", 4);
 
-        Mentoria mentoria1 = new Mentoria("Mentoria Java", "Descrição Mentoria Java", LocalDate.now());
-        Mentoria mentoria2 = new Mentoria("Mentoria JS", "Descrição Mentoria JS", LocalDate.now());
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Título curso java");
+        curso1.setDescricao("Descrição Curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Título curso js");
+        curso2.setDescricao("Descrição Curso js");
+        curso2.setCargaHoraria(9);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Título  Mentoria java");
+        mentoria1.setDescricao("Descrição Mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Título  Mentoria js");
+        mentoria2.setDescricao("Descrição Mentoria js");
+        mentoria2.setData(LocalDate.now());
 
         System.out.println(curso1);
         System.out.println(curso2);
-
-        System.out.println("\n" + mentoria1);
+        System.out.println(mentoria1);
         System.out.println(mentoria2);
+
 
     }
 
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,31 +2,13 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-   private String titulo;
-   private String descricao;
+public class Mentoria extends Conteudo {
+
    private LocalDate data;
 
-   public Mentoria(String titulo, String descricao, LocalDate data) {
-      this.titulo = titulo;
-      this.descricao = descricao;
-      this.data = data;
-   }
-
-   public String getTitulo(){
-      return titulo;
-   }
-
-   public void setTitulo(){
-      this.titulo = titulo;
-   }
-
-   public String getDescricao(){
-      return descricao;
-   }
-
-   public void setDescricao(){
-      this.descricao = descricao;
+   @Override
+   protected double calcularXp() {
+      return XP_PADRAO + 20d;
    }
 
    public LocalDate getData(){
@@ -40,9 +22,10 @@ public class Mentoria {
    @Override
    public String toString() {
       return "Mentoria{" +
-              "titulo='" + titulo + '\'' +
-              ", descricao='" + descricao + '\'' +
-              ", data=" + data +
+              "titulo='" + getTitulo() + '\'' +
+              ", descricao='" + getDescricao() + '\'' +
+              "data=" + data +
               '}';
+
    }
 }
