32205ad0d9ced9700b75b4cfc0dcbdadb6d20d7a
Edson Mizuma <edson.mizuma@gmail.com>
2024-07-17 15:21:37 -0300

    Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.time.LocalDate;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso("Curso Java", "Descrição curso Java", 8);
+        Curso curso2 = new Curso("Curso JS", "Descrição curso JS", 4);
+
+        Mentoria mentoria1 = new Mentoria("Mentoria Java", "Descrição Mentoria Java", LocalDate.now());
+        Mentoria mentoria2 = new Mentoria("Mentoria JS", "Descrição Mentoria JS", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        System.out.println("\n" + mentoria1);
+        System.out.println(mentoria2);
+
     }
+
+
 }
